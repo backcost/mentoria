f915df37465cbf82c9f42916f1eac6f0bcfbf297
Gabriel Backcost <gabriel_costa_ferreira@hotmail.com>
2021-02-11 00:42:57 -0300

    bom dia

---
+++ a04ex01.js
@@ -0,0 +1,10 @@
+// 1 – Calcule a média de diversas notas digitadas pelo usuário. 
+// O número de notas pode variar. O usuário vai usar o comando 
+// "fim" para dizer quando as notas acabaram e o calculo da média 
+// pode ser feita.
+
+
+// Entrada => n notas
+// Processamento =>
+// 1. Colocar notas em array
+// Saída => média das notas
