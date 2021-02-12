f6fa3ac49cd8f0c60b487a95929cdca4986fa382
Gabriel Backcost <gabriel_costa_ferreira@hotmail.com>
2021-02-11 23:27:21 -0300

    square a number

--- a04ex02.js
+++ a04ex02.js
@@ -1,32 +1,21 @@
-// 1 – Calcule a média de diversas notas digitadas pelo usuário. 
-// O número de notas pode variar. O usuário vai usar o comando 
-// "fim" para dizer quando as notas acabaram e o calculo da média 
-// pode ser feita.
+// 2 – Faça um programa que peça números e imprima o quadrado de cada número.
 
-
-// Entrada => n notas
+// Entrada => n números
 // Processamento =>
-// 1. readlineSync para receber notas
-// 2. Criar variaveis para somar as notas e para contar
-// 3. Usar o while
-// Saída => média das notas
+// 1. readlineSync para receber números
+// 2. Usar o while para mostrar o quadrado
+// Saída => quadrado de cada número
 
 let readlineSync = require('readline-sync')
 
 let run = true
-let sum = 0
-let c = 0
 
 while(run) {
-    let input = readlineSync.question('Digite uma nota: ')
+    let input = readlineSync.question('Digite um numero: ')
     
     if (input === 'fim') {
         run = false
         break
     } 
-    sum += Number(input)
-    c++
+    console.log(`O quadrado de ${input} é ${input**2}.`)
 }
-
-
-console.log(`A media das notas inseridas é ${sum/c}.`)
