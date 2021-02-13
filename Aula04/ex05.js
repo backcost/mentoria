781d5108fcefa334e20e9deffc2dc0d4af7ad3fe
Gabriel Backcost <gabriel_costa_ferreira@hotmail.com>
2021-02-13 12:39:35 -0300

    man or woman? who is the tallest?

--- Aula04/ex05.js
+++ Aula04/ex05.js
@@ -7,15 +7,40 @@
 
 // Entrada => altura e sexo de 5 pessoas
 // Processamento =>
-// 1. Separar dia, mês e ano
-// Saída => print 
+// 1. Pedir altura e sexo do usuário
+// 2. While para cobrir erros na digitão do sexo
+// 3. Condicional para contar homens e mulheres
+// 4. Condicional para ver qual é o mais alto
+// Saída => 
+// 1. print quantas são do sexo masculino e quantas são mulheres
+// 2. mostrar qual a maior altura e o sexo dessa pessoa
 
 
 const readlineSync = require('readline-sync')
+let women = 0
+let men = 0
+let tallest = 0
+let tallestgender = 0
 
-let input = readlineSync.question('Digite uma data (dd/mm/aaaa): ')
-day = input[0] + input[1]
-month = input[3] + input[4]
-year = input[6] + input[7] + input[8] + input[9]
+for (count = 1;count < 6; count++) {
+    let height = readlineSync.question(`Digite a altura (cm) da pessoa ${count}: `)
+    let gender = readlineSync.question(`Homem ou Mulher? `).toLowerCase()
+    while(gender != 'homem' && gender != 'mulher') {
+        gender = readlineSync.question(`Opcao invalida. Digite homem ou mulher: `).toLowerCase()
+    }
+    if (gender === 'homem') {
+        men++
+    } else {
+        women++
+    } if (height > tallest){
+        tallest = height
+        tallestgender = gender
+    }
 
-console.log(`Dia ${day}; Mês ${month}; Ano ${year}`)
+}
+console.log('----------------------------------')
+console.log(`${men} homem(ns);`)
+console.log(`${women} mulher(es);`)
+console.log(`A maior altura é ${tallest}cm e a pessoa é ${tallestgender};`)
+
+// E se mais de uma pessoa tiver a mesma altura?
