586ec582b354fd608d8f71616ec772bc55f0ce22
Gabriel Backcost <gabriel_costa_ferreira@hotmail.com>
2021-02-14 12:58:10 -0300

    days since 01/01/2017

---
+++ Aula07/ex04.js
@@ -0,0 +1,17 @@
+// 4.  Encontre o número de dias entre hoje e 1 de Janeiro de 2017.
+
+
+// Entrada => data de hoje e data desejada
+// Processamento =>
+// 1. Colocar data de hoje e data desejada em variaveis
+// 2. Subtrair data de hoje pela data desejada
+// Saída => número de dias entre hoje e a data desejada
+
+
+var oneDayMiliSeconds = 24*60*60*1000
+var today = Date.parse(new Date())
+var past = Date.parse(new Date("01/01/2017"))
+var days = (today - past)/(oneDayMiliSeconds)
+
+console.log(`Já se passaram ${days.toFixed(0)-1} dias desde 01/01/2017;`)
+console.log(`Hoje é o dia ${days.toFixed(0)}!`)
