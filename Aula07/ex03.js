72e1da6276a4567cd44be5f752d90a9c052a989e
Gabriel Backcost <gabriel_costa_ferreira@hotmail.com>
2021-02-14 12:57:35 -0300

    days since 01/01/2017

---
+++ Aula07/ex03.js
@@ -0,0 +1,8 @@
+// 3. Crie uma função para verificar se uma data inserida 
+// pela usuário é uma data válida. Dado a entrada do usuário 
+// você deve dizer se ela é uma data válida ou não.
+
+
+// Entrada => 
+// Processamento => Passar o dia d hoje do UTC para BRST
+// Saída => Data de hoje horário de Brasília
