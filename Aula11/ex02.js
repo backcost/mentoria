ee3c22a3dc36cf3a421b9014fe987bd9158e5d1d
Gabriel Backcost <gabriel_costa_ferreira@hotmail.com>
2021-02-19 10:37:43 -0300

    isArray

---
+++ Aula11/ex02.js
@@ -0,0 +1,9 @@
+// 2.Escreva uma função que recebe um array e retorna 
+// o primeiro elemento do array se nenhum outro argumento 
+// for passado, porém se outro argumento for passado ela vai 
+// retornar os "n" primeiros elementos do array.
+
+
+// Entrada => array
+// Processamento =>
+// Saída =>
