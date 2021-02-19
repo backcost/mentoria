dd31d7b193faca9a2249da37c2b2486c7efd54af
Gabriel Backcost <gabriel_costa_ferreira@hotmail.com>
2021-02-19 11:29:03 -0300

    array first elements

--- Aula11/ex02.js
+++ Aula11/ex02.js
@@ -6,4 +6,21 @@
 
 // Entrada => array
 // Processamento =>
-// Saída =>
+// 1. Função que recebe o primeiro argumento como array e um
+// segundo argumento que já começa com um valor
+// Saída => o primeiro elemento do array, porém se outro argumento 
+// for passado ela vai retornar os "n" primeiros elementos
+
+
+const first = (array, n = 1) => {
+    let temporaryArray = []
+    for(let index = 0; index < n; index++) {
+        temporaryArray.push(array[index])
+    }
+    return temporaryArray
+}
+
+console.log(first([1,2,3])) // => 1
+console.log(first([1,2,3], 2)) // => [1,2]
+console.log(first([1,2,3], 3)) // => [1,2,3]
+console.log(first([1,4,8,9,12], 1)) // => [1,4,8,9]
